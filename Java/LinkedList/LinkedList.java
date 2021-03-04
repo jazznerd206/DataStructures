f08d0742dcab05ef3a9d9923ec32f4b536dc1725
Andrew Miller <jazznerd206@icloud.com>
2021-03-04 11:24:54 -0800

    add and remove at index MOSTLY complete, some small error in the remove function. also two helpers, check index and traverse

--- Java/LinkedList/LinkedList.java
+++ Java/LinkedList/LinkedList.java
@@ -1,3 +1,4 @@
+import java.util.StringJoiner;
 
 class LinkedList {
     private Node head;
@@ -5,10 +6,17 @@ class LinkedList {
     private int size;
 
     // inner class
-    private class Node {
-        String data; // element
-        Node next; // next pointer
-        // Node prev; // previous pointer
+    class Node {
+        String data;
+        Node next;
+        // Node prev;
+
+        // CONSTRUCTOR
+        Node(String data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
     };
 
     // initialize linked list with null head, null tail and size zero
@@ -19,10 +27,9 @@ class LinkedList {
     }
 
     // HELPER FUNCTION
-    // throws index out of bounds exception, otherwise pass
+    // throws index out of bounds exception, otherwise continues
     public void checkIndex(int index, int low, int high) {
         if (index > high || index < low) {
-            System.out.println("Out of bounds");
             throw new IndexOutOfBoundsException(index + "");
         } else {
             System.out.println("Index valid");
@@ -34,54 +41,78 @@ class LinkedList {
     public Node traverse(int index) {
         // grab the head
         Node current = head;
-        // loop until the index before the one you want to insert
-        for (int i = 0; i < index - 1; ++i) {
+        // loop until the index you want to insert at
+        for (int i = 0; i < index; ++i) {
             current = current.next;
         }
         return current;
     }
 
     // ADD AT METHOD
+    // add an element to a list at the index from the params
+    // returns nothing
     public void addAt(int index, String element) {
 
-        // HELPER FUNCTION
+        // HELPER CHECK INDEX FUNCTION (lns 31-38)
         checkIndex(index, 0, size);
 
         // create new node, append passed element to data param
-        Node newNode = new Node();
-        newNode.data = element;
+        Node newNode = new Node(element, null);
 
         // if the list is empty, set the head to the new node
         // set the tail to the head since there is only 1 element
         // increment size
-        if (size() == 0) {
+        if (head == null) {
             head = newNode;
             tail = head;
             size++;
+            return;
             // if index is zero, we are creating a new head
             // grab the value of the head and set it to newNode.next
             // set newNode to the new head
             // increment size
-        } else if (index == 0) {
+        } else if (head != null && index == 0) {
             newNode.next = head;
             head = newNode;
             size++;
+            return;
         }
         ;
-
-        // HELPER FUNCTION
-        Node current = traverse(index);
+        // HELPER TRAVERSE FUNCTION (lns 42-52)
         // set newNode.next to "previous".next
-        newNode.next = current.next;
-        // set '"previous'.next to newNode
-        current.next = newNode;
+        // set "previous".next to newNode
         // increment size
+        Node current = traverse(index - 1);
+        newNode.next = current.next;
+        current.next = newNode;
         size++;
         return;
     }
 
+    // REMOVE METHOD
+    // remove element from list at index from params
+    // returns the removed element as a string
     public String remove(int index) {
-        return "";
+        Node response;
+        // HELPER CHECK INDEX FUNCTION (lns 31-38)
+        checkIndex(index, 0, size - 1);
+
+        // IF INDEX FROM PARAMS IS ZERO (HEAD)
+        if (index == 0) {
+            response = head;
+            head = head.next;
+            size--;
+        }
+        // call helper traverse function to get to right list position
+        // set the response to the current (response is the node from which we will send
+        // the data)
+        // stitch the hole
+        // decrement size
+        Node current = traverse(index - 1);
+        response = current.next;
+        current.next = current.next.next;
+        size--;
+        return response.data;
     }
 
     public int size() {
@@ -92,11 +123,20 @@ class LinkedList {
         StringBuilder sb = new StringBuilder();
         sb.append('{');
         Node curr = null;
-        // add the first fence post
+        // FENCE POST
+        // /-|-|-|-|-|-/
+        // /🐑🐑🐑🐑🐑🐑 /
+        // /🐑🐑🐑🐑🐑🐑 /
+        // /-|-|-|-|-|-/
         if (head != null) {
             sb.append(head.data);
             curr = head.next;
         }
+        // END FIRST POST
+        // ===================
+        // comma is place first with a space (type string because it's more than 1
+        // character)
+        // avoids the trailing comma!!
         while (curr != null) {
             sb.append(", ");
             sb.append(curr.data);
@@ -107,12 +147,59 @@ class LinkedList {
     }
 
     public static void main(String[] args) {
-        // test our your linked list here for now
+        System.out.println("==============================");
+        System.out.println("1. Init list");
+        System.out.println("==============================");
         LinkedList list = new LinkedList();
-        System.out.println("Hello World!");
+        System.out.println("==============================");
+        System.out.println("2. add three strings");
+        System.out.println("==============================");
         list.addAt(0, "string");
-        list.addAt(1, "string");
-        list.addAt(2, "string");
+        list.addAt(1, "string2");
+        list.addAt(2, "string3");
+        System.out.println("==============================");
+        System.out.println("list should have length of 3");
+        System.out.println("print list size");
+        System.out.println(list.size());
+        System.out.println("print list");
         System.out.println(list.toString());
+        System.out.println("==============================");
+        System.out.println("==============================");
+        System.out.println("3. remove from list at index 1");
+        System.out.println("==============================");
+        list.remove(1);
+        System.out.println("==============================");
+        System.out.println("list should have length of 2");
+        System.out.println("print list size");
+        System.out.println(list.size());
+        System.out.println("print list");
+        System.out.println(list.toString());
+        System.out.println("==============================");
+        System.out.println("==============================");
+        System.out.println("4. add to list at index 1, add new head at index 0");
+        System.out.println("==============================");
+        list.addAt(1, "string2 addback");
+        list.addAt(0, "string addback at index 0");
+        System.out.println("==============================");
+        System.out.println("list length should be 4");
+        System.out.println("print list size");
+        System.out.println(list.size());
+        System.out.println("print list");
+        System.out.println(list.toString());
+        System.out.println("==============================");
+        System.out.println("==============================");
+        System.out.println("5. remove all from list");
+        System.out.println("==============================");
+        list.remove(0);
+        list.remove(0);
+        list.remove(0);
+        list.remove(0);
+        System.out.println("==============================");
+        System.out.println("list length should be 0");
+        System.out.println("print list size");
+        System.out.println(list.size());
+        System.out.println("print list");
+        System.out.println(list.toString());
+        System.out.println("==============================");
     }
 }
