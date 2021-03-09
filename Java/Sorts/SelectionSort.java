3b9af427c1b13776def3d13429ce90ecb7ff605e
Andrew Miller <jazznerd206@icloud.com>
2021-03-09 10:38:28 -0800

    selection sort complete, PROOFREAD NEXT TIME

--- Java/Sorts/SelectionSort.java
+++ Java/Sorts/SelectionSort.java
@@ -1,19 +1,22 @@
 public class SelectionSort {
 
-    public <T extends Comparable<T>> void sort(T[] a) {
-        int l = a.length - 1;
-        for (int i = 0; i < l - 1; l++) {
+    public <T extends Comparable<T>> T[] sort(T[] a) {
+        int l = a.length;
+        for (int i = 0; i < l - 1; i++) {
             int mIndex = i;
             for (int j = i + 1; j < l; j++) {
                 System.out.printf("%s, %s, %s\n", mIndex, a[j], a[mIndex]);
                 printArray(a);
-                if (a[j].compareTo(a[mIndex]) <= 0)
+                if (a[j].compareTo(a[mIndex]) < 0)
                     mIndex = j;
             }
-            T k = a[mIndex];
-            a[mIndex] = a[i];
-            a[i] = k;
+            if (mIndex != i) {
+                T k = a[mIndex];
+                a[mIndex] = a[i];
+                a[i] = k;
+            }
         }
+        return a;
     }
 
     public static <T extends Comparable<T>> void printArray(T[] a) {
@@ -25,7 +28,7 @@ public class SelectionSort {
     public static void main(String[] args) {
         Integer[] arr = { 4, 23, 6, 78, 1, 54, 231, 9, 12 };
         SelectionSort selectionSort = new SelectionSort();
-        selectionSort.sort(arr);
-        printArray(arr);
+        Integer[] sorted = selectionSort.sort(arr);
+        printArray(sorted);
     }
 }
