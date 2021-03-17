fc77d34f86c8dace5c5a77dc8c526184ff231deb
Andrew Miller <jazznerd206@icloud.com>
2021-03-17 07:17:55 -0700

    add complete min heap

--- Java/Heap/BinaryHeap.java
+++ Java/Heap/BinaryHeap.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.NoSuchElementException;
+import java.util.Random;
 
 public class BinaryHeap {
 
@@ -28,7 +30,7 @@ public class BinaryHeap {
 
     // push()
     public void push(int value) {
-        if (size + 1 > data.length / 2) {
+        if (size == data.length) {
             grow();
         }
         data[size] = value;
@@ -41,10 +43,10 @@ public class BinaryHeap {
         int pidx = index / 2;
         while (index >= 1) {
             if (data[index] > data[pidx]) {
-                System.out.println("bubble break");
+                // System.out.println("bubble break");
                 break;
             } else {
-                System.out.println("swap");
+                // System.out.println("swap");
                 swap(pidx, index);
                 percolateUp(pidx);
             }
@@ -55,18 +57,26 @@ public class BinaryHeap {
     }
 
     // pop()
+
+    // pop() returns the top element from the heap and removes it from the heap
+    // heap maintains heap property after pop
+    // the first element in the heap must be the min element
+    // to do this, you store the top element of the heap in a return variable
+    // next, you replace the top element with the last element in the heap
+    // then you percolate down
+    // ret = arr[0]
+    // arr[0] = arr[last element position]
+    // decrement size
+    // percolate down moves the top element down to where it belongs
+
     public int pop() {
         if (size == 0) {
             throw new NoSuchElementException("no such element");
         }
         int value = data[0];
-        int i = 0;
-        while (i < size) {
-            data[i] = data[i + 1];
-            i++;
-        }
         size--;
-        System.out.println("Value: " + value);
+        data[0] = data[size];
+        percolateDown(0);
         return value;
     }
 
@@ -78,6 +88,22 @@ public class BinaryHeap {
 
     // percolate down
     public void percolateDown(int index) {
+        int temp = index;
+        int leftChild = 2 * index;
+        int rightChild = 2 * index + 1;
+
+        if (leftChild <= size && data[leftChild] < data[temp]) {
+            temp = leftChild;
+        }
+
+        if (rightChild <= size && data[rightChild] < data[temp]) {
+            temp = rightChild;
+        }
+
+        if (temp != index) {
+            swap(index, temp);
+            percolateDown(temp);
+        }
     }
 
     // swap
@@ -89,24 +115,43 @@ public class BinaryHeap {
 
     // grow
     public void grow() {
-        System.out.println("grow");
+        // System.out.println("grow");
         Integer[] newData = new Integer[data.length * 2];
-        for (int i = 0; i < size + 1; i++) {
+        for (int i = 0; i < size; i++) {
             newData[i] = data[i];
         }
         data = newData;
     }
 
-    public static int leftChild(int i) {
-        return 2 * i + 1;
-    }
+    // public static int leftChild(int i) {
+    // return 2 * i + 1;
+    // }
 
-    public static int rightChild(int i) {
-        return 2 * i + 2;
-    }
+    // public static int rightChild(int i) {
+    // return 2 * i + 2;
+    // }
 
-    private int parent(int pos) {
-        return pos / 2;
+    // private int parent(int pos) {
+    // return pos / 2;
+    // }
+
+    public void sort() {
+        int n = data.length;
+
+        // Build heap (rearrange array)
+        // for (int i = n / 2 - 1; i >= 0; i--)
+        // heapify(arr, n, i);
+
+        // One by one extract an element from heap
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = data[0];
+            data[0] = data[i];
+            data[i] = temp;
+
+            // call max heapify on the reduced heap
+            // heapify(sort, i, 0);
+        }
     }
 
     // print
@@ -120,29 +165,23 @@ public class BinaryHeap {
     // main
     public static void main(String[] args) {
         BinaryHeap bh = new BinaryHeap();
-        bh.push(-400);
-        bh.print();
-        bh.push(66);
-        bh.print();
-        bh.push(2);
-        bh.print();
-        bh.push(-8);
-        bh.print();
-        bh.push(1);
-        bh.print();
-        bh.push(90);
-        bh.print();
-        bh.push(31);
-        bh.print();
-        bh.push(17);
-        bh.print();
-        bh.push(5);
-        bh.print();
-        bh.pop();
-        bh.print();
-        bh.pop();
-        bh.print();
-        // bh.pop();
-        // bh.pop();
+        Random rand = new Random();
+        int[] arr = new int[rand.nextInt(21) + 5];
+        for (int i = 0; i < arr.length; i++) {
+            Integer n = rand.nextInt(Integer.MAX_VALUE);
+            bh.push(n);
+            arr[i] = n.intValue();
+        }
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        sb.append(bh.pop());
+        for (int i = 1; i < arr.length; i++) {
+            sb.append(' ');
+            sb.append(bh.pop());
+        }
+        sb.append(']');
+        System.out.println("heap: " + sb.toString());
+        System.out.println("array: " + Arrays.toString(arr));
     }
 }
--- Java/Heap/HeapSort.java
+++ Java/Heap/HeapSort.java
@@ -1,3 +1,41 @@
 public class HeapSort {
 
+    public void sort(int arr[]) {
+        int n = arr.length;
+
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // One by one extract an element from heap
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    void heapify(int data[], int n, int i) {
+        int largest = i;
+        int leftChild = 2 * i + 1;
+        int rightChild = 2 * i + 2;
+
+        if (leftChild < n && data[leftChild] > data[largest])
+            largest = leftChild;
+
+        if (rightChild < n && data[rightChild] > data[largest])
+            largest = rightChild;
+
+        if (largest != i) {
+            int temp = data[i];
+            data[i] = data[largest];
+            data[largest] = temp;
+
+            heapify(data, n, largest);
+        }
+    }
 }
