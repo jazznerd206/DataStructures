aa074c874e059001d5ab1d36aca6fbfd4b0ea3b2
Andrew Miller <jazznerd206@icloud.com>
2021-03-17 07:31:54 -0700

    pretty print pretty please

--- Java/Heap/BinaryHeap.java
+++ Java/Heap/BinaryHeap.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.NoSuchElementException;
 import java.util.Random;
 
 public class BinaryHeap {
@@ -28,7 +27,6 @@ public class BinaryHeap {
         this.data = new Integer[8];
     }
 
-    // push()
     public void push(int value) {
         if (size == data.length) {
             grow();
@@ -38,40 +36,21 @@ public class BinaryHeap {
         size++;
     }
 
-    // percolate up
     public void percolateUp(int index) {
         int pidx = index / 2;
         while (index >= 1) {
             if (data[index] > data[pidx]) {
-                // System.out.println("bubble break");
                 break;
             } else {
-                // System.out.println("swap");
                 swap(pidx, index);
                 percolateUp(pidx);
             }
         }
-        // if (index == 1 || data[pidx] < data[index]) {
-        // return;
-        // }
     }
 
-    // pop()
-
-    // pop() returns the top element from the heap and removes it from the heap
-    // heap maintains heap property after pop
-    // the first element in the heap must be the min element
-    // to do this, you store the top element of the heap in a return variable
-    // next, you replace the top element with the last element in the heap
-    // then you percolate down
-    // ret = arr[0]
-    // arr[0] = arr[last element position]
-    // decrement size
-    // percolate down moves the top element down to where it belongs
-
     public int pop() {
-        if (size == 0) {
-            throw new NoSuchElementException("no such element");
+        if (isEmpty()) {
+            throw new IllegalStateException("You cannot pop from an empty heap");
         }
         int value = data[0];
         size--;
@@ -80,13 +59,6 @@ public class BinaryHeap {
         return value;
     }
 
-    public int extractMin() {
-        int value = data[size];
-        size--;
-        return value;
-    }
-
-    // percolate down
     public void percolateDown(int index) {
         int temp = index;
         int leftChild = 2 * index;
@@ -106,16 +78,13 @@ public class BinaryHeap {
         }
     }
 
-    // swap
     public void swap(int l, int r) {
         int temp = data[l];
         data[l] = data[r];
         data[r] = temp;
     }
 
-    // grow
     public void grow() {
-        // System.out.println("grow");
         Integer[] newData = new Integer[data.length * 2];
         for (int i = 0; i < size; i++) {
             newData[i] = data[i];
@@ -123,38 +92,22 @@ public class BinaryHeap {
         data = newData;
     }
 
-    // public static int leftChild(int i) {
-    // return 2 * i + 1;
-    // }
-
-    // public static int rightChild(int i) {
-    // return 2 * i + 2;
-    // }
-
-    // private int parent(int pos) {
-    // return pos / 2;
-    // }
-
-    public void sort() {
-        int n = data.length;
-
-        // Build heap (rearrange array)
-        // for (int i = n / 2 - 1; i >= 0; i--)
-        // heapify(arr, n, i);
-
-        // One by one extract an element from heap
-        for (int i = n - 1; i > 0; i--) {
-            // Move current root to end
-            int temp = data[0];
-            data[0] = data[i];
-            data[i] = temp;
-
-            // call max heapify on the reduced heap
-            // heapify(sort, i, 0);
-        }
+    public static int leftChild(int i) {
+        return 2 * i + 1;
     }
 
-    // print
+    public static int rightChild(int i) {
+        return 2 * i + 2;
+    }
+
+    private int parent(int pos) {
+        return pos / 2;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
     public void print() {
         for (int i = 0; i < this.data.length; i++) {
             System.out.printf("%d, ", this.data[i]);
@@ -162,7 +115,6 @@ public class BinaryHeap {
         System.out.println("");
     }
 
-    // main
     public static void main(String[] args) {
         BinaryHeap bh = new BinaryHeap();
         Random rand = new Random();
@@ -177,7 +129,7 @@ public class BinaryHeap {
         sb.append('[');
         sb.append(bh.pop());
         for (int i = 1; i < arr.length; i++) {
-            sb.append(' ');
+            sb.append(" -||- ");
             sb.append(bh.pop());
         }
         sb.append(']');
