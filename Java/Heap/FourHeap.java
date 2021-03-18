aaa152341378a7673960d9c3b93090586ad508ce
Andrew Miller <jazznerd206@icloud.com>
2021-03-18 10:08:27 -0700

    four heap complete:

--- Java/Heap/FourHeap.java
+++ Java/Heap/FourHeap.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class FourHeap {
     int size;
     Integer[] data;
@@ -22,13 +25,46 @@ public class FourHeap {
         // System.out.println("bitch y tho");
         int pidx = (index - 1) / chCnt;
         int lastEl = data[index];
-        // System.out.println(data[index] + " " + data[pidx]);
         while (index >= 1 && lastEl < data[pidx]) {
             data[index] = data[pidx];
             index = pidx;
-            pidx = (pidx - 1) / 2;
+            pidx = (pidx - 1) / chCnt;
         }
         data[index] = lastEl;
+    }
+
+    public int pop() {
+        System.out.println("bang bang");
+        if (isEmpty()) {
+            throw new IllegalStateException("No crayons in this box.");
+        }
+        int value = data[0].intValue();
+        size--;
+        data[0] = data[size];
+        percolateDown(0);
+        return value;
+    }
+
+    public void percolateDown(int idx) {
+        int temp = data[idx];
+        int minChildIndex = getMinChild(idx * chCnt + 1, idx * chCnt + chCnt);
+        System.out.println(minChildIndex);
+        while (minChildIndex < size && data[minChildIndex] < data[idx]) {
+            data[idx] = data[minChildIndex];
+            idx = minChildIndex;
+            minChildIndex = getMinChild(minChildIndex * chCnt + 1, minChildIndex * chCnt + chCnt);
+        }
+        data[idx] = temp;
+    }
+
+    public int getMinChild(int from, int to) {
+        int minChild = from;
+        for (int i = from + 1; (i <= to && i < chCnt); i++) {
+            if (data[minChild] > data[i]) {
+                minChild = i;
+            }
+        }
+        return minChild;
     }
 
     public void swap(int l, int r) {
@@ -45,6 +81,10 @@ public class FourHeap {
         data = newData;
     }
 
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
     public void print() {
         for (int i = 0; i < this.data.length; i++) {
             System.out.printf("%d, ", this.data[i]);
@@ -54,38 +94,26 @@ public class FourHeap {
 
     public static void main(String[] args) {
         FourHeap fh = new FourHeap();
-        fh.push(5);
-        fh.push(1);
-        fh.push(2);
-        fh.push(8);
-        fh.print();
-        fh.push(35);
-        fh.push(15);
-        fh.push(85);
-        fh.push(14);
-        fh.print();
-        fh.push(15);
-        fh.push(17);
-        fh.push(86);
-        fh.push(12);
-        fh.print();
-        // Random rand = new Random();
-        // int[] arr = new int[rand.nextInt(21) + 5];
-        // for (int i = 0; i < arr.length; i++) {
-        // Integer n = rand.nextInt(Integer.MAX_VALUE);
-        // fh.push(n);
-        // arr[i] = n.intValue();
-        // }
-        // Arrays.sort(arr);
-        // StringBuilder sb = new StringBuilder();
-        // sb.append('[');
-        // sb.append(fh.pop());
-        // for (int i = 1; i < arr.length; i++) {
-        // sb.append(" -||- ");
-        // sb.append(fh.pop());
-        // }
-        // sb.append(']');
-        // System.out.println("heap: " + sb.toString());
-        // System.out.println("array: " + Arrays.toString(arr));
+        Random rand = new Random();
+        int[] arr = new int[rand.nextInt(21) + 5];
+        for (int i = 0; i < arr.length; i++) {
+            Integer n = rand.nextInt(Integer.MAX_VALUE);
+            fh.push(n);
+            arr[i] = n.intValue();
+        }
+        for (int i = 0; i < fh.data.length; i++) {
+            System.out.println(" : " + fh.data[i]);
+        }
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        sb.append(fh.pop());
+        for (int i = 1; i < arr.length; i++) {
+            sb.append(" : ");
+            sb.append(fh.pop());
+        }
+        sb.append(']');
+        System.out.println("heap: " + sb.toString());
+        System.out.println("array: " + Arrays.toString(arr));
     }
 }
