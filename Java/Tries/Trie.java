d55fe85b7ba8745a6f391d3940e944d84d9bb12a
Andrew Miller <jazznerd206@icloud.com>
2021-03-25 11:30:34 -0700

    t9 predictive trie complete

--- Java/Tries/Trie.java
+++ Java/Tries/Trie.java
@@ -1,5 +1,5 @@
 import java.io.File;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,6 +9,10 @@ public class Trie {
 
     Node root;
 
+    Trie() {
+        this.root = new Node();
+    }
+
     // T9 to possible word (0-9 input, list of words output)
 
     public class Node {
@@ -17,86 +21,62 @@ public class Trie {
 
         public Node() {
             this.words = new LinkedList();
-            this.m = null;
+            this.m = new HashMap();
         }
     }
 
     public void insert(int[] t9, String word) {
-        for (int i = 0; i < t9.length; i++) {
-            System.out.print("T9: ");
-            System.out.print(t9[i] + " ");
-            System.out.println();
-        }
-        System.out.println("word: " + word);
-        // start at the root
         Node curr = root;
-        // initialize loop through t9 array, numerical representation of the letters
         for (int i = 0; i < t9.length; i++) {
-            char c = word.charAt(i);
-            System.out.println("current character: " + c);
-            System.out.println("current: " + curr);
-            // if map m of trie contains the current key of the t9 array
-            if (!curr.m.containsKey(t9[i])) {
-                // build a new node
-                // is it enough just to put it in the map?
-                curr = new Node();
-                curr.words.add(word);
-                curr.m.put(t9[i], curr);
-                System.out.println("word not found: " + curr);
-            } else {
-                // at this point, we know the map contains a node corresponding to the current
-                // t9 digit
-                // get that node, set curr to its value
-                curr = curr.m.get(c);
-                System.out.println("word found: " + curr);
+            Integer d = t9[i];
+            if (!curr.m.containsKey(d)) {
+                curr.m.put(d, new Node());
             }
+            curr = curr.m.get(d);
         }
+        curr.words.add(word);
         return;
     }
 
     public List<String> lookup(int[] t9) {
-        return Arrays.asList(new String[] { "bar", "cap", "car" });
+        Node curr = root;
+        for (int i = 0; i < t9.length; i++) {
+            Integer d = t9[i];
+            if (!curr.m.containsKey(d)) {
+                return Collections.emptyList();
+            }
+            curr = curr.m.get(d);
+        }
+        return curr.words;
+    }
+
+    private static int charToT9(char ch) {
+        if ('a' <= ch && ch <= 'c')
+            return 2;
+        if ('d' <= ch && ch <= 'f')
+            return 3;
+        if ('g' <= ch && ch <= 'i')
+            return 4;
+        if ('j' <= ch && ch <= 'l')
+            return 5;
+        if ('m' <= ch && ch <= 'o')
+            return 6;
+        if ('p' <= ch && ch <= 's')
+            return 7;
+        if ('t' <= ch && ch <= 'v')
+            return 8;
+        if ('w' <= ch && ch <= 'z')
+            return 9;
+        return 0;
     }
 
     // return t9 #num representation
     public static int[] toT9(String word) {
-        System.out.println("length: " + word.toCharArray().length);
-        int length = word.toCharArray().length;
-        int[] T9 = new int[length];
-        HashMap<Character, Integer> map = new HashMap();
-        map.put('a', 2);
-        map.put('b', 2);
-        map.put('c', 2);
-        map.put('d', 3);
-        map.put('e', 3);
-        map.put('f', 3);
-        map.put('g', 4);
-        map.put('h', 4);
-        map.put('i', 4);
-        map.put('j', 5);
-        map.put('k', 5);
-        map.put('l', 5);
-        map.put('m', 6);
-        map.put('n', 6);
-        map.put('o', 6);
-        map.put('p', 7);
-        map.put('q', 7);
-        map.put('r', 7);
-        map.put('s', 7);
-        map.put('t', 8);
-        map.put('u', 8);
-        map.put('v', 8);
-        map.put('w', 9);
-        map.put('x', 9);
-        map.put('y', 9);
-        map.put('z', 9);
-        System.out.println(map);
-        for (int i = 0; i < length; i++) {
-            char key = word.charAt(i);
-            System.out.println("Key: " + key + ", from map: " + map.get(key));
-            T9[i] = map.get(key);
+        int[] t9 = new int[word.length()];
+        for (int i = 0; i < t9.length; i++) {
+            t9[i] = charToT9(word.charAt(i));
         }
-        return T9;
+        return t9;
     }
 
     // http://github.com/dwyl/english-words/blob/master/words.txt
@@ -119,6 +99,14 @@ public class Trie {
         return true;
     }
 
+    public static int[] toIntArray(String input) {
+        int[] t9 = new int[input.length()];
+        for (int i = 0; i < t9.length; i++) {
+            t9[i] = input.charAt(i) - '0';
+        }
+        return t9;
+    }
+
     public static void main(String[] args) {
         try {
             File dictionary = new File("/Users/miller/Downloads/words.txt");
@@ -135,22 +123,22 @@ public class Trie {
             }
             scan.close();
 
-            // Scanner input = new Scanner(System.in);
-            // String line = "";
-            // while (true) {
-            // System.out.print("Enter T9: ");
-            // line = input.nextLine();
-            // line = line.trim().toLowerCase();
-            // if (line.startsWith("q")) {
-            // break;
-            // }
-            // String token = line.split(" ")[0];
-            // if (isValidT9(token)) {
-            // List<String> words = t9ToWords.lookup(toT9(token));
-            // System.out.println("Possible words are: " + words.toString());
-            // }
-            // }
-            // input.close();
+            Scanner input = new Scanner(System.in);
+            String line = "";
+            while (true) {
+                System.out.print("Enter T9: ");
+                line = input.nextLine();
+                line = line.trim().toLowerCase();
+                if (line.startsWith("q")) {
+                    break;
+                }
+                String token = line.split(" ")[0];
+                if (isValidT9(token)) {
+                    List<String> words = t9ToWords.lookup(toIntArray(token));
+                    System.out.println("Possible words are: " + words.toString());
+                }
+            }
+            input.close();
         } catch (Exception ex) {
             System.out.println(ex.toString());
         }
